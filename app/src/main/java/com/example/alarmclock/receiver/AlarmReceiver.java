a08557069719c4a086e8f4ae96871819775f9c3c
tiot07 <tiot7155@gmail.com>
2020-07-23 16:43:34 +0900

    整理

--- app/src/main/java/com/example/alarmclock/receiver/AlarmReceiver.java
+++ app/src/main/java/com/example/alarmclock/receiver/AlarmReceiver.java
@@ -26,30 +26,28 @@ public class AlarmReceiver extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-        LocalDateTime date1 = LocalDateTime.now();
-        DateTimeFormatter dtformat1 = DateTimeFormatter.ofPattern("HH");
-        DateTimeFormatter dtformat2 = DateTimeFormatter.ofPattern("mm");
-        String hour = dtformat1.format(date1);
-        String minute = dtformat2.format(date1);
-        int minutes = Integer.parseInt(minute);
-
-        String alarmTime = hour + ":"
-                + String.format("%02d", minutes + 1);
-
-
-        ListItem listItem = new ListItem();
-
-        listItem.setAlarmID(minutes + 1);
-        listItem.setAlarmName("無題");
-        listItem.setTime(alarmTime);
-        Util.setAlarm(context, listItem);
-
+        //動いているかのflagを取得
         String flag = readFile(context, fileName);
         Intent startActivityIntent = new Intent(context, WakeUpActivity.class);
         startActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
+        if (Integer.parseInt(flag) == 0) {
+            //時刻を取得
+            LocalDateTime date1 = LocalDateTime.now();
+            DateTimeFormatter dtformat1 = DateTimeFormatter.ofPattern("HH");
+            DateTimeFormatter dtformat2 = DateTimeFormatter.ofPattern("mm");
+            String hour = dtformat1.format(date1);
+            String minute = dtformat2.format(date1);
+            int minutes = Integer.parseInt(minute);
+            String alarmTime = hour + ":"
+                    + String.format("%02d", minutes + 1);
 
-        if (Integer.parseInt(flag) != 1) {
+            //1分後のアラームをセット
+            ListItem listItem = new ListItem();
+            listItem.setAlarmID(minutes + 1);
+            listItem.setAlarmName("無題");
+            listItem.setTime(alarmTime);
+            Util.setAlarm(context, listItem);
             context.startActivity(startActivityIntent);
         }
     }
